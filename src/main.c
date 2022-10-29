fa53406a44ca36a93fde1516833d2828bd488922
NaomiChann <80477294+NaomiChann@users.noreply.github.com>
2022-10-29 18:59:25 -0200

    soutouts to stack overflow user 4pie0

--- src/main.c
+++ src/main.c
@@ -1,86 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdint.h>
 
 typedef struct {
-	int validBit, tag;
+	uint32_t tag;
+	int val;
 } Cache;
 
-int main( int argc, char *argv[] ) {
-	/*if ( argc != 7 ) {
-		printf( "Numero de argumentos incorreto. Utilize: \n" );
-		printf( "./cache_simulator <nsets> <bsize> <assoc> <substituicao> <flag_saida> arquivo_de_entrada \n" );
-		exit( EXIT_FAILURE );
-	}
+uint32_t reverse_bytes( uint32_t bytes );
 
-	int nsets = atoi( argv[1] ), bsize = atoi( argv[2] ), assoc = atoi( argv[3] );
-	char *subst = argv[4];
-	int flagOut = atoi( argv[5] );
-	FILE *inputFile = argv[6];
-	*/
-	int nsets = 256, bsize = 4, assoc = 1;
+int main() {
+	// variables
+	int miss = 0, hit = 0, missC = 0, accesses = 0;
+	float missRate = 0, hitRate = 0;
+	size_t addressSize = 4; // 4 bytes = 32 bits
+
+	// args
+	int nSets = 256, bSize = 1, assoc = 1, flagOut = 1;
 	char *subst = "r";
-	int flagOut = 1;
-	FILE *inputFile = fopen( "C:/Internet Explorer/The Basement/spaghetti code/cache sim/output/bin_100.bin", "rb" );
+	FILE *inputFile;
 
-	int address, tag, index;
-	int offsetBits, indexBits, tagBits, accesses = 0;
-	float miss = 0, hit = 0, compulsoryMiss = 0;
-	float hitRate, missRate, compulsoryMissRate, capacityMissRate, conflictMissRate;
+	// setting values
+	int bitsOffset = ( int ) log2( bSize );
+	int bitsIndex = ( int ) log2( nSets );
+	int bitsTag = 32 - bitsOffset - bitsIndex;
+
+	// current address info
+	uint32_t tag, index, address, mask = 0xffffffff >> bitsTag;
+
+	// creates empty cache
 	Cache *cache;
-
-	int buffer[1];
-
-	printf( "nsets = %d \n", nsets );
-	printf( "bsize = %d \n", bsize );
-	printf( "assoc = %d \n", assoc );
-	printf( "subst = %s \n", subst );
-	printf( "flagOut = %d \n", flagOut );
-	printf( "arquivo = %s \n", inputFile );
-
-	cache = ( Cache * ) malloc( ( nsets * assoc ) * sizeof( Cache ) );
-
-	for ( int i = 0; i < ( nsets * assoc ); i++ ) {
-		cache[i].validBit = 0;
+	cache = ( Cache * ) malloc( ( nSets * assoc ) * sizeof( Cache ) );
+	
+	// sets all valid bits to 0
+	for ( int i = 0; i < ( nSets * assoc ); i++ ) {
+		cache[i].val = 0;
 	}
-	//criar uma estrutura de dados para armazenar os tags e os bits de validade.
-	offsetBits = log2( bsize );
-	indexBits = log2( nsets );
-	tagBits = 32 - offsetBits - indexBits;
-	//descobre o número de bits de cada parcela do address
-	//para todos os addresss do arquivo
-	int i = 0;
-	while ( fgetc( inputFile ) != EOF ) {
+
+	fopen_s( &inputFile, "C:/Internet Explorer/The Basement/spaghetti code/cache sim/output/bin_100.bin", "rb" );
+
+	//fseek( inputFile, 3, SEEK_SET );
+	//while ( !feof( inputFile ) ) {
+	while ( fread( &address, addressSize, 1, inputFile ) != 0 ) {
+		//fread( &address, addressSize, 1, inputFile );
 		accesses++;
-		address = fread( buffer, sizeof( buffer ), 1, inputFile );
-		tag = address >> ( offsetBits + indexBits );
-		index = ( address >> offsetBits );
-		index = index >> ( 2^( indexBits - 1 ) );
-		//isso é uma máscara que vai deixar apenas os bits do índice na variável “address”.
-		// para o mapeamento direto
-		if ( cache[index].validBit == 0 ) {
-			compulsoryMiss++;
-			miss++;
-			cache[index].validBit = 1;
+		address = reverse_bytes( address );
+		tag = address >> ( bitsOffset + bitsIndex );
+		index = ( address & mask );
+		index = index >> bitsOffset;
+
+		if ( cache[index].val == 0 ) {
+			missC++;
+			cache[index].val = 1;
 			cache[index].tag = tag;
-			// estas duas últimas instruções representam o tratamento da falta.
 		} else if ( cache[index].tag == tag ) {
 			hit++;
 		} else {
 			miss++;
-			//conflito ou capacidade?
-			cache[index].validBit = 1;
+			cache[index].val = 1;
 			cache[index].tag = tag;
 		}
-		i++;
 	}
+	
+	miss += missC;
+	missRate = ( float ) miss / accesses;
+	hitRate = ( float ) hit / accesses;
 
-	missRate = ( miss / accesses );
-	hitRate = ( hit / accesses );
+	printf( "%d %d %d %d \n", accesses, hit, miss, missC );
+	printf( "%2f, %2f \n", hitRate, missRate );
 
-	if ( flagOut == 1 ) {
-		printf( "%d, %f, %f, f, f, f \n", accesses, hitRate, missRate );
-	}
+	free( cache );
+	fclose( inputFile );
 
 	return ( 0 );
+}
+
+uint32_t reverse_bytes( uint32_t bytes ) {
+    uint32_t aux = 0;
+    uint8_t byte;
+    int i;
+
+    for ( i = 0; i < 32; i += 8 ) {
+        byte = ( bytes >> i ) & 0xff;
+        aux |= byte << ( 32 - 8 - i );
+    }
+
+    return ( aux );
 }
