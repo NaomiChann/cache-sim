6f80aa881ae3131e6558a6aa2a5de6872168b160
NaomiChann <80477294+NaomiChann@users.noreply.github.com>
2022-10-31 20:26:17 -0200

    everything seems right except what clearly isnt

--- src/main.c
+++ src/main.c
@@ -15,13 +15,13 @@ uint32_t reverse_bytes( uint32_t bytes );
 int main() {
 	// variables
 	int miss = 0, hit = 0, missComp = 0, missConf = 0, missCap = 0, accesses = 0;
-	float missRate = 0, hitRate = 0;
+	float missRate = 0, hitRate = 0, missCompRate = 0, missConfRate = 0, missCapRate = 0;
 	size_t addressSize = 4; // 4 bytes = 32 bits
-	srand( time( NULL ) ); // randomize seed
+	srand( time( NULL ) ); // randomize seed	
 
 	// args (debug)
-	int nSets = 256, bSize = 1, assoc = 2; //, flagOut = 1;
-	char subst[2] = "f";
+	int nSets = 512, bSize = 8, assoc = 1, flagOut = 0;
+	char subst[2] = "r";
 	FILE *inputFile;
 
 	// setting values
@@ -30,14 +30,14 @@ int main() {
 	int bitsTag = 32 - bitsOffset - bitsIndex;
 
 	// current address info
-	uint32_t tag, index, address, mask = 0xffffffff >> bitsTag;
+	uint32_t tag, index, address;
 
 	// creates empty cache
 	Cache *cache;
 	cache = ( Cache * ) malloc( ( nSets * assoc ) * sizeof( Cache ) );
 	
 	// helper
-	int *dummy, elWiwi = 0, helperGnome = 0;
+	int *dummy, aux = 0, auxS = 0, auxM = nSets * assoc;
 	dummy = ( int * ) malloc( ( nSets * assoc ) * sizeof( int ) );
 
 	// sets all allocated arrays' values to 0
@@ -46,7 +46,7 @@ int main() {
 		dummy[i] = 0;
 	}
 	// file path (debug, needs to be changed if running elsewhere)
-	fopen_s( &inputFile, "C:/Internet Explorer/The Basement/spaghetti code/cache sim/output/bin_100.bin", "rb" );
+	fopen_s( &inputFile, "C:/Internet Explorer/The Basement/spaghetti code/cache sim/output/vortex.in.sem.persons.bin", "rb" );
 
 	while ( fread( &address, addressSize, 1, inputFile ) != 0 ) { // loops until eof
 		accesses++;
@@ -54,7 +54,7 @@ int main() {
 
 		// bitwise manipulation shenanigans
 		tag = address >> ( bitsOffset + bitsIndex );
-		index = ( address & mask );
+		index = address % nSets;
 		index = index >> bitsOffset;
 
 		// this has never happened before
@@ -62,69 +62,80 @@ int main() {
 			printf( "wha? why? how? how do i even stop this?" );
 		}
 
-		for ( int i = 0; i < assoc; i++ ) {
-			if ( cache[index + ( nSets * ( i ) )].val == 0 ) {
+		for ( int i = 0; i < assoc; i++ ) { // checks every block
+			if ( cache[index + ( nSets * i )].val == 0 ) {
 				missComp++;
-				cache[index + ( nSets * ( i ) )].val = 1;
-				cache[index + ( nSets * ( i ) )].tag = tag;
-				helperGnome = 1; // sets helper to 1 since block was already properly updated
-				break;
-			} else if ( cache[index + ( nSets * ( i ) )].tag == tag ) {
+				auxM--;
+				cache[index + ( nSets * i )].val = 1;
+				cache[index + ( nSets * i )].tag = tag;
+				goto end;
+			} else if ( cache[index + ( nSets * i )].tag == tag ) {
 				hit++;
 				if ( strcmp( subst, "l" ) == 0 ) { // treats lru case
-					for ( int j = 0; j < assoc; j++ ) { // increases "use value" for all other slots
-						dummy[index + ( nSets * ( j ) )] += 1;
+					for ( int j = 0; j < nSets; j++ ) { // increases "use value" for all other slots
+						dummy[index + ( nSets * j )] += 1;
 					}
-					dummy[index + ( nSets * ( i ) )] = 0; // sets its use value to 0 since it's new
+					dummy[index + ( nSets * i )] = 0; // sets its use value to 0 since it's new
 				}
-				helperGnome = 1; // hoo
-				break;
+				goto end;
 			}
 		}
 
-		// miss cases substitution behaviors
-		if ( helperGnome != 1 ) {
-			miss++; // no clue on how to differentiate between capacity and conflict
-
-			// random
-			if ( strcmp( subst, "r" ) == 0 ) {
-				cache[index + ( nSets * ( rand() % ( assoc - 1 ) ) )].tag = tag;
-			// fifo
-			} else if ( strcmp( subst, "f" ) == 0 ) {
-				cache[elWiwi].tag = tag;
-				if ( elWiwi < assoc ) { // increases the helper variable up to the associativity
-					elWiwi++;
-				} else { // then loops back to 0
-					elWiwi = 0;
-				}
-			// lru
-			} else if ( strcmp( subst, "l" ) == 0 ) {
-				elWiwi = index;
-				for ( int i = 1; i < assoc; i++ ) { // checks every single one for their use value and keeps the highest
-					if ( dummy[index + ( nSets * ( i ) )] > dummy[index + ( nSets * ( i - 1 ) )] ) {
-						elWiwi = index + ( nSets * ( i ) ); // an auxiliar array keeps track of every slot
-					}
-				}
-
-				for ( int i = 0; i < assoc; i++ ) { // increases the use value of everyone else in this block
-					dummy[index + ( nSets * ( i ) )] += 1;
-				}
-				cache[elWiwi].tag = tag; // sets the previously highest as the new address
-				dummy[elWiwi] = 0; // abd its use value to 0
-			}
+		// other miss cases substitution behaviors
+		if ( auxM == 0 ) {
+			missCap++;
+		} else {
+			missConf++;
 		}
 
-		helperGnome = 0; // resets the helper value
+		// random
+		if ( strcmp( subst, "r" ) == 0 ) {
+			cache[index + ( nSets * ( rand() % ( assoc - 1 ) ) )].tag = tag;
+		// fifo
+		} else if ( strcmp( subst, "f" ) == 0 ) {
+			cache[auxS].tag = tag;
+			if ( auxS < assoc ) { // increases the helper variable up to the associativity
+				auxS++;
+			} else { // then loops back to 0
+				auxS = 0;
+			}
+		// lru
+		} else if ( strcmp( subst, "l" ) == 0 ) {
+			aux = index;
+			for ( int i = 1; i < assoc; i++ ) { // checks every single one for their use value and keeps the highest
+				if ( dummy[index + ( nSets * ( i ) )] > dummy[index + ( nSets * ( i - 1 ) )] ) {
+					aux = index + ( nSets * ( i ) ); // an auxiliar array keeps track of every slot
+				}
+			}
+
+			for ( int i = 0; i < assoc; i++ ) { // increases the use value of everyone else in this block
+				dummy[index + ( nSets * ( i ) )] += 1;
+			}
+			cache[aux].tag = tag; // sets the previously highest as the new address
+			dummy[aux] = 0; // abd its use value to 0
+		}
+
+		end:
 	}
 	
-	// the miss values updating and calculation
+	// miss values updating and calculation
 	miss += missComp + missConf + missCap;
+
 	missRate = ( float ) miss / accesses;
 	hitRate = ( float ) hit / accesses;
 
-	printf( "%d %d %d %d \n", accesses, hit, miss, missComp );
-	printf( "%2f, %2f \n", hitRate, missRate );
+	missCompRate = ( float ) missComp / miss;
+	missConfRate = ( float ) missConf / miss;
+	missCapRate = ( float ) missCap / miss;
 
+	if ( flagOut == 1 ) {
+		printf( "%d %.2f %.2f %.2f %.2f %.2f \n", accesses, hitRate, missRate, missCompRate, missCapRate, missConfRate );
+	} else {
+		printf( "%d sets, block size: %d, associativity: %d \n", nSets, bSize, assoc );
+		printf( "accesses: %d hits: %d misses: %d \nhit rate: %.2f \nmiss rate: %.2f \n", accesses, hit, miss, hitRate, missRate );
+		printf( "compulsory: %d rate: %.2f \nconflict: %d rate: %.2f \ncapacity: %d rate %.2f \n", missComp, missCompRate, missConf, missConfRate, missCap, missCapRate );
+	}
+	
 	// properly closes and frees everything
 	free( cache );
 	free( dummy );
